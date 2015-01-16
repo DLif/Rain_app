813203e5c15e667824f3a3832165128dbc3d0dcd
MauriceTheObese <denchikg@gmail.com>
2015-01-15 23:24:10 -0800

    reuploaded app

---
+++ App8/App8/coolPage.xaml.cs
@@ -0,0 +1,235 @@
+﻿using App8.Common;
+using App8.DataModel;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Services.Maps;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace App8
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+
+   
+
+
+   
+
+
+    public sealed partial class coolPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        private MobileServiceCollection<PathGroup, PathGroup> groupsTest;
+        private IMobileServiceTable<PathGroup> groupsTable = App.mobileClient.GetTable<PathGroup>();
+        private MobileServiceUser user;
+
+        private async System.Threading.Tasks.Task Authenticate()
+        {
+            while (user == null)
+            {
+                string message ="boom";
+                try
+                {
+                    user = await App.mobileClient
+                        .LoginAsync(MobileServiceAuthenticationProvider.Facebook);
+                    message =
+                        string.Format("You are now logged in - {0}", user.UserId);
+                }
+                  
+                catch (InvalidOperationException)
+                {
+                    message = "You must log in. Login Required";
+                }
+                catch
+                {
+
+                }
+
+               // MessageDialog dialog = new MessageDialog(message);
+              //  await dialog.ShowAsync();
+
+            }
+        }
+
+        public coolPage()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+          // await AuthenticateAsync();
+
+
+            PathGroup pt = new PathGroup();
+
+            pt.GroupName = pathName.Text;
+            byte[] arr = new byte[4];
+            arr[0] = 17;
+            pt.DestinationPoint = arr;
+            pt.SourcePoint = arr;
+
+            await  groupsTable.InsertAsync(pt);
+
+            var result = await this.groupsTable.ToCollectionAsync();
+
+
+           
+
+            list.ItemsSource = result;
+            
+
+        }
+
+        private async System.Threading.Tasks.Task AuthenticateAsync()
+        {
+            while (user == null)
+            {
+                string message;
+                try
+                {
+                    user = await App.mobileClient
+                        .LoginAsync(MobileServiceAuthenticationProvider.Facebook);
+                    message =
+                        string.Format("You are now logged in - {0}", user.UserId);
+                }
+                catch (InvalidOperationException)
+                {
+                    message = "You must log in. Login Required";
+                }
+
+                var dialog = new MessageDialog(message);
+                dialog.Commands.Add(new UICommand("OK"));
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+               
+            
+        }
+
+        private void testBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App8.DataModel.MapUtils.testClass temp = new App8.DataModel.MapUtils.testClass();
+            App8.DataModel.MapUtils.testClass temp2 = new App8.DataModel.MapUtils.testClass();
+
+            temp.Num = 5;
+            temp.Text = "lol";
+            temp2.Num = 16;
+            temp2.Text = "faf";
+
+            temp.Obj = temp2;
+
+
+            ObjectSerializer srz = new ObjectSerializer();
+
+            byte[] arr = srz.ObjectToByteArray(temp);
+
+            App8.DataModel.MapUtils.testClass res = srz.ByteArrayToObject(arr);
+
+             
+           
+
+        }
+    }
+}
