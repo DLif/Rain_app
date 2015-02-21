4a090bef007f6db228de8f17543bbb9322f0e1e5
stradivarius10 <pooyae@mail.tau.ac.il>
2015-02-21 23:41:06 +0200

    Server Side update
    Rain Amount controller for Denis

---
+++ server_side/MobileServiceFinal/MobileServiceFinal/Models/PixelsSeriallizer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MobileServiceFinal.Models
+{
+
+    public class RainApiSerializer
+    {
+        public static String SerializeRequest(APIRequest request)
+        {
+            var serializer = new DataContractSerializer(typeof(APIRequest));
+            var memStream = new MemoryStream();
+            serializer.WriteObject(memStream, request);
+            var buffer = memStream.ToArray();
+            return Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        }
+        public static APIRequest DeserializeRequest(String input)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(input);
+            var memStream = new MemoryStream(buffer);
+            var jsonSeriazlier = new DataContractSerializer(typeof(APIRequest));
+            return (APIRequest)jsonSeriazlier.ReadObject(memStream);
+        }
+
+
+        public static String demorun()
+        {
+            List<PixelRep> a = new List<PixelRep>();
+            a.Add(new PixelRep(6, 7));
+            a.Add(new PixelRep(8, 9));
+            APIRequest x = new APIRequest(a);
+            return SerializeRequest(x);
+        }
+    }
+    [DataContract]
+
+    public class PixelRep
+    {
+        [DataMember]
+        // NOTE: this is the column index
+        public int X { get; set; }
+        [DataMember]
+        // NOTE: this is the row index
+        public int Y { get; set; }
+        public PixelRep(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    [DataContract]
+    public class APIRequest
+    {
+        [DataMember]
+        public List<PixelRep> Pixels { get; set; }
+        public APIRequest(List<PixelRep> pixels)
+        {
+            this.Pixels = pixels;
+        }
+    }
+}
+
